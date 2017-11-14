d2fce4872cb276d5524f528fa9e63ce4ef771b6d
Meiogordo <gamer-go@hotmail.com>
2017-11-14 19:01:43 +0000

    Changed beaches container, added getAllConselhos, getBeachesByConselho and fixed displayAllBeaches

--- src/LeisureGuide.h
+++ src/LeisureGuide.h
@@ -6,13 +6,21 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <set>
+#include <algorithm>
 
 class LeisureGuide {
 private:
-	multimap<string, Beach *> beaches;
+	vector<pair<string, Beach*>> beaches;
 	vector<Restaurant> restaurants;
 	vector<POI> POIs;
 	vector<Lodging> lodging;
+	/**
+	 * @brief Gets all the conselhos where there are beaches
+	 * @return Returns a vector of all the Conselhos
+	 */
+	vector<string> getAllConselhos() const;
+	vector<Beach*> getBeachesByConselho(const string &conselho) const;
 public:
 	/**
 	* @brief Default constructor to allow creating an empty Leisure Guide that will receive the options afterwards
@@ -22,7 +30,7 @@ public:
 	* @brief Constructs a Leisure Guide with a multimap containing all beaches and the respective county and no POIs, restaurants or lodging, they may be added later
 	* @param	 multimap containing all beaches and the respective county
 	*/
-	LeisureGuide(multimap<string, Beach *> beaches);
+	LeisureGuide(const vector<pair<string, Beach*>> &beaches);
 	/**
 	* @brief Constructs a Leisure Guide with a multimap containing all beaches and the respective county as well as vectors, each containing the avaliable POIs, restaurants and lodging
 	* @param beaches		multimap containing all beaches and the respective county in the area
@@ -30,7 +38,7 @@ public:
 	* @param POIs			vector containing avaliable points of interest (POIs) in the surrounding area
 	* @param lodging		vector containing avaliable lodging in the surrounding area
 	*/
-	LeisureGuide(multimap<string, Beach *> beaches, vector<Restaurant> restaurants, vector<POI> POIs, vector<Lodging> lodging);
+	LeisureGuide(const vector<pair<string, Beach*>> &beaches, const vector<Restaurant> &restaurants, const vector<POI> &POIs, const vector<Lodging> &lodging);
 	void displayAllBeaches() const;
 	void displayAllPOIs() const;
 	void displayAllRestaurants() const;
